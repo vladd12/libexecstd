a8a3919d3714329ebbcddff992877f05ed8a8aef
vladd12 <vladd12z32@gmail.com>
2023-01-14 22:00:55 +0300

    Index class big update
    - adding new compile time checks in detail namespace
    - adding constexpr operator for version in detail namespace
    - adding methods size(), dimension() and operator[]() in Index class
    - adding Index1D, Index2D and Index3D classes on base Index class in Util namespace
    - adding tests for checking this features

--- include/execstd/util/index.hpp
+++ include/execstd/util/index.hpp
@@ -2,47 +2,122 @@
 
 #include <array>
 #include <cstdint>
+#include <execstd/core/export.hpp>
 #include <type_traits>
 
 namespace detail
 {
-template <bool value> struct is_correct_args_num_base : std::false_type
+template <bool value> struct is_condition_true : std::false_type
 {
 };
 
-template <> struct is_correct_args_num_base<true> : std::true_type
+template <> struct is_condition_true<true> : std::true_type
 {
 };
 
-/// \brief Comparing implementation.
-template <std::size_t N, typename... Args> inline constexpr auto is_correct_args_num_impl = (N == sizeof...(Args));
+/// \brief Implementation of comparing the arguments count with a given N.
+template <std::size_t N, typename... Args> //
+inline constexpr auto is_correct_args_num_impl = (N == sizeof...(Args));
 
-/// \brief Checking the number of passed arguments in compile time.
-template <std::size_t N, typename... Args>
-using is_correct_args_num = is_correct_args_num_base<is_correct_args_num_impl<N, Args...>>;
+/// \brief Implementation of numeric comparing (is lhs more than rhs).
+template <std::size_t lhs, std::size_t rhs> //
+inline constexpr auto is_more_than_impl = (lhs > rhs);
 
-/// \brief Reduction for using.
-template <std::size_t N, typename... Args>
+/// \brief Using implementation of comparing the arguments count.
+/// \see is_correct_args_num_impl
+template <std::size_t N, typename... Args> //
+using is_correct_args_num = is_condition_true<is_correct_args_num_impl<N, Args...>>;
+
+/// \brief Using implementation of numeric comparing.
+/// \see is_more_than_impl
+template <std::size_t lhs, std::size_t rhs> //
+using is_more_than = is_condition_true<is_more_than_impl<lhs, rhs>>;
+
+// Inlining values for using.
+template <std::size_t N, typename... Args> //
 inline constexpr auto is_correct_args_num_v = is_correct_args_num<N, Args...>::value;
+template <std::size_t N> //
+inline constexpr auto is_more_than_zero_v = is_more_than<N, 0>::value;
+
+/// \brief Implementation of constexpr operator for version.
+template <class F, std::size_t... I> constexpr void for_constexpr_impl(F &&func, std::index_sequence<I...>)
+{
+    (func(std::integral_constant<std::size_t, I> {}), ...);
+}
+
+/// \brief Constexpr operator for version.
+/// \see for_constexpr_impl
+template <size_t C, class F> constexpr void for_constexpr(F &&func)
+{
+    for_constexpr_impl(std::forward<F>(func), std::make_index_sequence<C> {});
+}
 
 } // namespace detail
 
 namespace Util
 {
-/// \brief Templates class for representing the index base interface.
-template <typename T, std::size_t dim> class Index
+/// \brief Template class for representing the index base interface.
+template <typename T, std::size_t dim, std::enable_if_t<detail::is_more_than_zero_v<dim>, bool> = true> //
+class Index
 {
 public:
     using value_type = T;
-    value_type _indexes[dim];
+    using ref_type = T &;
+    using const_ref_type = const T &;
 
-    constexpr T &operator[](const std::size_t &index) const noexcept
+    value_type _indexes[dim];
+    static constexpr auto _dimension = dim;
+
+    constexpr void fill(const_ref_type value) noexcept
     {
-        if constexpr (index < dim)
-            return _indexes[dim];
+        for (std::size_t i = 0; i < _dimension; i++)
+            _indexes[i] = value;
+    }
+
+    constexpr const value_type size() const noexcept
+    {
+        value_type result = 1;
+        detail::for_constexpr<_dimension>([&](auto index) { result = result * _indexes[index]; });
+        return result;
+    }
+
+    constexpr const std::size_t dimension() const noexcept
+    {
+        return _dimension;
+    }
+
+    constexpr std::size_t dimension() noexcept
+    {
+        return _dimension;
+    }
+
+    constexpr const_ref_type operator[](const std::size_t &index) const noexcept
+    {
+        if (index < _dimension)
+            return _indexes[index];
         else
             return _indexes[0];
     }
+
+    constexpr ref_type operator[](const std::size_t &index) noexcept
+    {
+        if (index < _dimension)
+            return _indexes[index];
+        else
+            return _indexes[0];
+    }
+};
+
+class libapi Index1D : public Index<std::uint32_t, 1>
+{
+};
+
+class libapi Index2D : public Index<std::uint32_t, 2>
+{
+};
+
+class libapi Index3D : public Index<std::uint32_t, 3>
+{
 };
 
 } // namespace Util
--- tests/util_index.cpp
+++ tests/util_index.cpp
@@ -1,14 +1,14 @@
 #include <execstd/util/index.hpp>
 #include <gtest/gtest.h>
 
-TEST(Detail_IsCorrectArgsNum, PositiveCompileTime_Test)
+TEST(Detail_CompileTime, IsCorrectArgsNum_PositiveTest)
 {
     constexpr auto expected = true;
     constexpr auto actual = detail::is_correct_args_num_v<3, int, char, double>;
     ASSERT_EQ(expected, actual);
 }
 
-TEST(Detail_IsCorrectArgsNum, NegativeCompileTime_Test)
+TEST(Detail_CompileTime, IsCorrectArgsNum_NegativeTest)
 {
     using namespace detail;
     constexpr auto expected = false;
@@ -18,10 +18,61 @@ TEST(Detail_IsCorrectArgsNum, NegativeCompileTime_Test)
     ASSERT_EQ(expected, actualSecond);
 }
 
-TEST(Util_Index, TestCtor)
+TEST(Detail_CompileTime, IsMoreThanZero_Test)
 {
-    int n = 1;
-    Util::Index<int, 3> index3d { 1, 2, 3 };
-    index3d[n];
-    ASSERT_EQ(true, true);
+    using namespace detail;
+    constexpr auto positive = is_more_than_zero_v<1>;
+    constexpr auto negative = is_more_than_zero_v<0>;
+    ASSERT_EQ(positive, true);
+    ASSERT_NE(positive, negative);
+}
+
+TEST(Util_IndexClass, CtorTest)
+{
+    // Not empty initializer list (aggregate initialization)
+    constexpr std::size_t size = 3;
+    Util::Index<int, size> test_index1 = { 1, 2, 3 };
+    ASSERT_EQ(test_index1[0], 1);
+    ASSERT_EQ(test_index1[1], 2);
+    ASSERT_EQ(test_index1[2], 3);
+    // Empty initializer list
+    constexpr Util::Index<int, size> test_index2 = {};
+    for (std::size_t i = 0; i < size; i++)
+    {
+        ASSERT_EQ(test_index2[i], 0);
+    }
+}
+
+TEST(Util_IndexClass, DimensionTest)
+{
+    constexpr std::size_t expected1 = 3;
+    Util::Index<int, expected1> test_index1;
+    ASSERT_EQ(expected1, test_index1.dimension());
+    constexpr std::size_t expected2 = 25;
+    constexpr Util::Index<int, expected2> test_index2 {};
+    ASSERT_EQ(expected2, test_index2.dimension());
+}
+
+TEST(Util_IndexClass, FillTest)
+{
+    constexpr int expected = 5;
+    constexpr std::size_t size = 10;
+    Util::Index<int, size> test_index = {};
+    test_index.fill(expected);
+    for (std::size_t i = 0; i < size; i++)
+    {
+        ASSERT_EQ(expected, test_index[i]);
+    }
+}
+
+TEST(Util_IndexClass, SizeTest)
+{
+    constexpr Util::Index3D test_index1 = { 1, 2, 3 };
+    constexpr auto actual1 = test_index1.size();
+    constexpr decltype(actual1) expected1 = 1 * 2 * 3;
+    ASSERT_EQ(expected1, actual1);
+    Util::Index<int, 5> test_index2 = { 1, 1, 10, 10, 3 };
+    auto actual2 = test_index2.size();
+    constexpr auto expected2 = 300;
+    ASSERT_EQ(expected2, actual2);
 }
